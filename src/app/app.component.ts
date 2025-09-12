fa9ff79abf56acc3aa2955b8e4624a9d9ace65bd
pedro.souza <pedro.souza@martinellitech.com.br>
2025-09-12 17:23:48 -0300

    feat: corrigindo inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgIf } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -20,4 +20,5 @@ import { MatListModule } from '@angular/material/list';
 })
 export class AppComponent {
   opened = signal(false);
+  readonly currentYear = new Date().getFullYear();
 }
